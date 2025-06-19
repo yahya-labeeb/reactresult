ea6a496e55d434decbd138fde30c3a31bd875b60
yahya_labeeb <yahya_labeeb@hotmial.com>
2025-06-19 18:05:20 +0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,69 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Get the client
+const mysql2_1 = __importDefault(require("mysql2"));
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+// Create the connection to database
+const port = 3000;
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    // origin:"http://localhost:5173"
+    origin: 'https://student-exam-qzkl.vercel.app'
+}));
+app.use(express_1.default.json());
+//app.use("/styles", express.static(__dirname + '/styles'));
+/* const db =  mysql.createConnection({
+    user:'root',
+    host:'localhost',
+    password:'1234',
+    database:'reacttest'
+    
+}) */
+/* const db =  mysql.createPool({
+    user:'yahya',  //'sgroot'
+    host:'SG-yahya-12738-mysql-master.servers.mongodirector.com',
+    password:'Ya@123456za', // 'dmcUQ0ExpV0IB-Fg'
+    database:'reacttest'
+  
+}) */
+const db = mysql2_1.default.createPool({
+    user: 'freedb_Ya123456', //'sgroot'
+    host: 'sql.freedb.tech',
+    password: '$5hXe7V$Vgc5?Z2', // 'dmcUQ0ExpV0IB-Fg'
+    database: 'freedb_reacttest'
+});
+//Get all
+app.get('/', (req, res) => {
+    db.query('select * from test', async (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // const students= await res.send(result)
+            return res.send(result);
+        }
+        return res.send(result);
+    });
+});
+// Get one 
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM test WHERE `id`= ?";
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // const students= await res.send(result)
+            return res.send(result);
+        }
+        return res.send(result);
+    });
+});
+app.listen(port, () => {
+    console.log("running on por " + port);
+});
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+// Get the client
+import mysql from 'mysql2';
+import  express  from 'express';
+import cors from 'cors'
+// Create the connection to database
+const port=3000
+const app=express()
+app.use(cors({
+   // origin:"http://localhost:5173"
+  origin: 'https://student-exam-qzkl.vercel.app'
+ }))
+app.use(express.json())
+ //app.use("/styles", express.static(__dirname + '/styles'));
+/* const db =  mysql.createConnection({
+    user:'root',
+    host:'localhost',
+    password:'1234',
+    database:'reacttest'
+    
+}) */
+/* const db =  mysql.createPool({
+    user:'yahya',  //'sgroot'
+    host:'SG-yahya-12738-mysql-master.servers.mongodirector.com',
+    password:'Ya@123456za', // 'dmcUQ0ExpV0IB-Fg'
+    database:'reacttest'
+  
+}) */
+
+const db =  mysql.createPool({
+    user:'freedb_Ya123456',  //'sgroot'
+    host:'sql.freedb.tech',
+    password:'$5hXe7V$Vgc5?Z2', // 'dmcUQ0ExpV0IB-Fg'
+    database:'freedb_reacttest'
+  
+})
+
+
+
+
+
+//Get all
+app.get('/',(req,res)=>{
+    db.query('select * from test', async(err,result)=>{
+     if(err){
+        console.log(err)
+     }else{
+     // const students= await res.send(result)
+      return res.send(result)
+     }
+     return res.send(result)
+    })
+    }) 
+  
+  // Get one 
+   app.get('/:id', (req, res) => {
+     const id = req.params.id;
+     const sql = "SELECT * FROM test WHERE `id`= ?";
+     db.query(sql, [id], (err, result) => {
+        if(err){
+            console.log(err)
+         }else{
+         // const students= await res.send(result)
+          return res.send(result)
+         }
+         return res.send(result)
+     });
+   }); 
+  
+  
+app.listen(port,()=>{
+    console.log("running on por " +port)
+ })
+
